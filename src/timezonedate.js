a708d97a4cdd988987663fe459a1bf112e4e4d70
boblauer <rlauer@gmail.com>
2013-12-08 22:08:17 -0600

    Initial test framework in place.  Thats about it though.

---
+++ src/timezonedate.js
@@ -0,0 +1,3 @@
+function TimeZoneDate(offset, date) {
+
+}
---
+++ tests/tests.js
@@ -0,0 +1,9 @@
+var assert = chai.assert;
+
+describe('dummy test', function() {
+  it('should work', function(done) {
+    assert.equal(1, 1);
+    done();
+  });
+});
+
