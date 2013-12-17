5e348bd072cabc79198d8cb6ef45aea7840135fc
boblauer <rlauer@gmail.com>
2013-12-17 16:32:01 -0600

    Getting there, I think.

--- src/timezonedate.js
+++ src/timezonedate.js
@@ -1,3 +1,56 @@
 function TimeZoneDate(offset, date) {
+  this._offset = offset;
+  this._date = date ? new Date(date) : new Date();
 
+  this._date = this._getDateWithLocalOffsetAdded();
 }
+
+TimeZoneDate.prototype._getDateWithTargetOffsetAdded = function(date) {
+  var localOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  var targetOffset = this._offset * 60 * 1000;
+
+  var ms = +(date || this._date) + localOffset - targetOffset;
+  return new Date(ms);
+};
+
+TimeZoneDate.prototype._getDateWithLocalOffsetAdded = function(date) {
+  var localOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  var targetOffset = this._offset * 60 * 1000;
+
+  var ms = +(date || this._date) - localOffset + targetOffset;
+  return new Date(ms);
+};
+
+'getDate getDay getFullYear getHours getMilliseconds getMinutes getMonth getSeconds getTime getUTCDate getUTCDay getUTCFullYear getUTCHours getUTCMilliseconds getUTCMinutes getUTCMonth getUTCSeconds getYear'
+.split(' ').forEach(function(fn) {
+  if (Date.prototype[fn]) {
+    TimeZoneDate.prototype[fn] = function() {
+      var targetDate = this._getDateWithTargetOffsetAdded();
+      return Date.prototype[fn].apply(targetDate, arguments);
+    };
+  }
+});
+
+'setDate setFullYear setHours setMilliseconds setMinutes setMonth setSeconds setTime setUTCDate setUTCFullYear setUTCHours setUTCMilliseconds setUTCMinutes setUTCMonth setUTCSeconds setYear'
+.split(' ').forEach(function(fn) {
+  if (Date.prototype[fn]) {
+    TimeZoneDate.prototype[fn] = function() {
+      var getFn = fn.replace('set', 'get');
+
+      this._date = this._getDateWithTargetOffsetAdded();
+      Date.prototype[fn].apply(this._date, arguments);
+      this._date = this._getDateWithLocalOffsetAdded();
+
+      return this[getFn] ? this[getFn]() : this._date.valueOf();
+    };
+  }
+});
+
+TimeZoneDate.prototype.toString = function() {
+  var date = this._getDateWithTargetOffsetAdded();
+  return Date.prototype.toString.apply(date, arguments);
+};
+
+TimeZoneDate.prototype.valueOf = function() {
+  return this._date.valueOf();
+};
--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,51 @@
 var assert = chai.assert;
 
-describe('dummy test', function() {
-  it('should work', function(done) {
-    assert.equal(1, 1);
+describe('Testing basic functionality', function() {
+  var offset = new Date().getTimezoneOffset();
+
+  it('should be run with a sys clock of -0600 UTC', function(done) {
+    assert.equal(offset, 360);
+    done();
+  });
+
+  var localDate = new Date('1/1/2013 5:00 PM');
+  var utcDate = new TimeZoneDate(0, '1/1/2013 11:00 PM');
+
+  it('should create a new TimeZoneDate object', function(done) {
+    assert.ok(utcDate);
+    done();
+  });
+
+  it('should return the same value as a local date', function(done) {
+    assert.equal(utcDate.valueOf(), localDate.valueOf());
     done();
   });
 });
 
+describe('Test w/ target of UTC', function() {
+  var utcDate = new TimeZoneDate(0, '1/10/2013 17:40:30');
+
+  it('should have a time of 17:40:30.00', function(done) {
+    assert.equal(utcDate.getHours(), 17);
+    assert.equal(utcDate.getMinutes(), 40);
+    assert.equal(utcDate.getSeconds(), 30);
+    assert.equal(utcDate.getMilliseconds(), 0);
+    done();
+  });
+
+  it('should have a time of 14:45:23.00', function(done) {
+    utcDate.setHours(14, 45, 23, 12);
+    assert.equal(utcDate.getHours(), 14);
+    assert.equal(utcDate.getMinutes(), 45);
+    assert.equal(utcDate.getSeconds(), 23);
+    assert.equal(utcDate.getMilliseconds(), 12);
+    done();
+  });
+
+  it('should span days b/w local and target', function(done) {
+    utcDate.setHours(1, 0, 0, 0);
+    assert.equal(utcDate.getHours(), 1);
+    assert.equal(utcDate.toString(), utcDate._date.toString());
+    done();
+  });
+});
